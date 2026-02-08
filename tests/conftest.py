7e5da021c2c1aa6428d515bc46f06dc4a0702c66
Diego Carvallo <dccakes@gmail.com>
2026-02-08 21:31:11 +0000

    chore(tests): initial test scaffold

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import vcr
+from pydantic_settings import BaseSettings
+
+
+class TestSettings(BaseSettings):
+    vcr_cassette_library_dir: str = "tests/cassettes"
+    vcr_record_mode: str = "none"
+
+    class ConfigDict:
+        env_file = "envs/.env.test"
+        env_file_encoding = "utf-8"
+
+
+settings = TestSettings()
+
+vcr_instance: vcr.VCR = vcr.VCR(
+    cassette_library_dir=settings.vcr_cassette_library_dir,
+    record_mode=settings.vcr_record_mode,
+    match_on=["method", "scheme", "host", "port", "path", "query"],
+    filter_query_parameters=[],
+    decode_compressed_response=True,
+)
