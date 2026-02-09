b0bd8cdb4b44e5daae9364715bf8fb46d690bf4a
Diego Carvallo <dccakes@gmail.com>
2026-02-09 21:14:30 +0000

    feat(client): move query to pydantic model

--- src/client.py
+++ src/client.py
@@ -1,5 +1,31 @@
 import httpx
 from src.domain.models import SearchResponse
+from src.domain.enums import NpiEnumerationType
+from pydantic import BaseModel, Field
+
+
+class NpiSearchRequest(BaseModel):
+    version: str = "2.1"
+
+
+# limit: int = 10
+# skip: int = 0
+
+
+class NpiWhere(NpiSearchRequest):
+    npi_number: str | None = Field(
+        default=None, description="The NPI number", serialization_alias="number"
+    )
+    enumeration_type: NpiEnumerationType | None = None
+    taxonomy_description: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    organization_name: str | None = None
+    city: str | None = None
+    state: str | None = None
+    postal_code: str | None = None
+    country_code: str | None = None
+    address_purpose: str | None = None
 
 
 class NpiClient:
@@ -8,8 +34,8 @@ class NpiClient:
         self.base_url = "https://npiregistry.cms.hhs.gov/api/"
         self.version = "2.1"
 
-    async def lookup(self, npi_number: str) -> SearchResponse | None:
-        params = {"version": self.version, "number": npi_number}
+    async def lookup(self, request: NpiWhere) -> SearchResponse | None:
+        params = request.model_dump(exclude_none=True, by_alias=True)
         response = await self.client.get(self.base_url, params=params)
 
         if response.status_code != 200:
--- tests/unit/test_npi_client.py
+++ tests/unit/test_npi_client.py
@@ -3,13 +3,15 @@ import pytest
 from tests.conftest import vcr_instance
 from src.client import NpiClient
 from src.domain.models import NpiEnumerationType, IndividualProviderResponse
+from src.client import NpiWhere
 
 
 @pytest.mark.asyncio
 @vcr_instance.use_cassette("search_by_npi_number.yaml")
 async def test_search_by_npi_number():
     client = NpiClient()
-    result = await client.lookup("1114382983")
+    request = NpiWhere(npi_number="1114382983")
+    result = await client.lookup(request)
     assert result is not None
     assert result.result_count == 1
     assert len(result.results) == 1
